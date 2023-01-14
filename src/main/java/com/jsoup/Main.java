dfd7c6b155342cfd747964ef0749fe113c54bfec
miguel castro <miguel.castro9@hotmail.com>
2023-01-14 16:55:49 -0300

    convertendo string em html e recuperando dados do html convertido

--- src/main/java/com/jsoup/Main.java
+++ src/main/java/com/jsoup/Main.java
@@ -1,13 +1,31 @@
 package com.jsoup;
 
+import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 /**
  *
  * @author Miguel Castro
  */
 public class Main {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+
+        String html = "<html><head><title>JSOUP</title></head>"
+                + "<body><p>Convertendo string em HTML</p></body></html>";
+
+        Document doc = Jsoup.parse(html);
+
+        System.out.println("***************************");
+        System.out.println(doc);
+        System.out.println("***************************");
+        System.out.print("Recuperando tag <title>: ");
+        System.out.println(doc.getElementsByTag("title"));
+        System.out.print("Recuperando tag <p>: ");
+        System.out.println(doc.getElementsByTag("p").get(0).text());
+        System.out.println("***************************");
+
         
     }
 }
