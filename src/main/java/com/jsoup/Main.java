ffb46c3332d55ba561ecaef01cd39989df6ff365
miguel castro <miguel.castro9@hotmail.com>
2023-01-14 17:43:32 -0300

    gerando arquivo index.html atraves da conversao

--- src/main/java/com/jsoup/Main.java
+++ src/main/java/com/jsoup/Main.java
@@ -1,6 +1,7 @@
 package com.jsoup;
 
 import java.io.IOException;
+import java.io.PrintStream;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
@@ -13,10 +14,12 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         String html = "<html><head><title>JSOUP</title></head>"
-                + "<body><p>Convertendo string em HTML</p></body></html>";
+                + "<body><p>Convertendo string em HTML.</p>"
+                + "<p>Criando um arquivo index.html através da string convertida.</p>"
+                + "</body></html>";
 
         Document doc = Jsoup.parse(html);
-
+        
         System.out.println("***************************");
         System.out.println(doc);
         System.out.println("***************************");
@@ -25,6 +28,8 @@ public class Main {
         System.out.print("Recuperando tag <p>: ");
         System.out.println(doc.getElementsByTag("p").get(0).text());
         System.out.println("***************************");
+        PrintStream printStream = new PrintStream("index.html");
+        printStream.print(doc);
 
         
     }
